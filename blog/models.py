a13d1a73b1344ce2fa6bc3d84a9b115b9ab44f08
Brogan McShane <mcshane-b4@ulster.ac.uk>
2020-06-22 11:06:50 +0100

    Blog Model Commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=25)
+    myDate = models.DateTimeField()
+    myPost = models.TextField(max_length = 300)
+    mySnapshot = models.ImageField(upload_to = 'images/')
